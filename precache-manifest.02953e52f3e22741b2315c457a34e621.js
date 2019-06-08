0d666fa43f38072a31796e71a57c03579c2ab5d5
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2019-06-08 18:48:34 +0300

    Updates

---
+++ precache-manifest.02953e52f3e22741b2315c457a34e621.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c22f9adee93ae30559ec0c4c8da4a4",
+    "url": "/goit-react-hw-01/index.html"
+  },
+  {
+    "revision": "13caafb763ede7ac08fa",
+    "url": "/goit-react-hw-01/static/css/main.544ba674.chunk.css"
+  },
+  {
+    "revision": "c31b5ecc3c8d85e17e7d",
+    "url": "/goit-react-hw-01/static/js/2.44e0a5b2.chunk.js"
+  },
+  {
+    "revision": "13caafb763ede7ac08fa",
+    "url": "/goit-react-hw-01/static/js/main.a8c87171.chunk.js"
+  },
+  {
+    "revision": "f51632aec18d5636ad74",
+    "url": "/goit-react-hw-01/static/js/runtime~main.bdf556df.js"
+  }
+]);
